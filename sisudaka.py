87f8d184d5434aed232e41990a99f1f46ed36766
shniubobo <shniubobo@outlook.com>
2021-01-24 06:01:30 +0800

    Bump version to 0.2.0

--- sisudaka.py
+++ sisudaka.py
@@ -16,7 +16,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-__version__ = '0.1.0+dev'
+__version__ = '0.2.0'
 
 from datetime import datetime, time
 from functools import wraps
